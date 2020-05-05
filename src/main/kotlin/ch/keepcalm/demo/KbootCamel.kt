80915f7f2fda67a9b638b9c8645fcf52520e02a0
Marcel Widmer <marzelwidmer@gmail.com>
2020-05-05 10:09:04 +0200

    Add FileBuilderRoutes

--- src/main/kotlin/ch/keepcalm/demo/KbootCamel.kt
+++ src/main/kotlin/ch/keepcalm/demo/KbootCamel.kt
@@ -1,11 +1,14 @@
 package ch.keepcalm.demo
 
+import org.apache.camel.builder.RouteBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class KbootCamel
 
 fun main(args: Array<String>) {
-	runApplication<KbootCamel>(*args)
+    runApplication<KbootCamel>(*args)
 }
+
