adf95a6812d896c7932b847ba183dc89048d9a9d
Marcel Widmer <marzelwidmer@gmail.com>
2020-05-05 13:33:11 +0200

    XPATH date

--- src/main/kotlin/ch/keepcalm/demo/KbootCamel.kt
+++ src/main/kotlin/ch/keepcalm/demo/KbootCamel.kt
@@ -1,9 +1,7 @@
 package ch.keepcalm.demo
 
-import org.apache.camel.builder.RouteBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class KbootCamel
---
+++ src/main/kotlin/ch/keepcalm/demo/processor/HeaderProcessor.kt
@@ -0,0 +1,28 @@
+package ch.keepcalm.demo.processor
+
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+import org.apache.camel.language.xpath.XPathBuilder
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+class HeaderProcessor : Processor {
+
+    private final val XPATH_DATE = "/order/orderDate/text()"
+
+    override fun process(exchange: Exchange?) {
+        val oderXml = exchange?.`in`?.body
+        val orderDateTime = XPathBuilder.xpath(XPATH_DATE).evaluate(exchange?.context, oderXml)
+        val formattedOrderDate = getFormattedData(orderDateTime = orderDateTime)
+        exchange?.`in`?.setHeader("orderDate", formattedOrderDate)
+    }
+
+    // TODO: 05.05.20 DirtyHarry Implementation
+    private fun getFormattedData(orderDateTime: String): String {
+        val datetime = LocalDateTime.ofInstant(Instant.parse(orderDateTime), ZoneOffset.UTC)
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(datetime)
+    }
+
+}
