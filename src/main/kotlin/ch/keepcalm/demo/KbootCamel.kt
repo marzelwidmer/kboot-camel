c40cbaf6f1547fddf3fd93141e92f30b93dd19f8
Marcel Widmer <marzelwidmer@gmail.com>
2020-05-05 07:53:17 +0200

    Initial Commit

---
+++ src/main/kotlin/ch/keepcalm/demo/KbootCamel.kt
@@ -0,0 +1,11 @@
+package ch.keepcalm.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KbootCamel
+
+fun main(args: Array<String>) {
+	runApplication<KbootCamel>(*args)
+}
